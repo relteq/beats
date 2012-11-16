467279088ab8aae7863eea78d1bfadfb37a87ff8
Elena Dorogush <dorogush@gmail.com>
2012-11-16 14:25:00 +0400

    default types [db init]

--- src/main/java/edu/berkeley/path/beats/db/Admin.java
+++ src/main/java/edu/berkeley/path/beats/db/Admin.java
@@ -28,13 +28,16 @@ package edu.berkeley.path.beats.db;
 
 import java.io.File;
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.sql.Connection;
 import java.sql.SQLException;
 
 import org.apache.log4j.Logger;
 import org.apache.torque.TorqueException;
 import org.apache.torque.util.BasePeer;
+import org.apache.torque.util.Transaction;
 
-import edu.berkeley.path.beats.om.Projects;
+import edu.berkeley.path.beats.om.*;
 import edu.berkeley.path.beats.simulator.SiriusErrorLog;
 import edu.berkeley.path.beats.simulator.SiriusException;
 
@@ -85,17 +88,7 @@ public class Admin {
 				System.err);
 		logger.info("Database tables created");
 
-		try {
-			Projects db_project = new Projects();
-			db_project.setId(Long.valueOf(0));
-			db_project.setName("default");
-			db_project.save();
-			logger.info("Project 'default' [id=1] created");
-		} catch (TorqueException exc) {
-			throw new SiriusException(exc);
-		} catch (Exception exc) {
-			throw new SiriusException(exc);
-		}
+		new Initializer().run();
 		logger.info("Database " + params.getDBName() + " initialized");
 	}
 
@@ -155,4 +148,128 @@ public class Admin {
 			}
 		}
 	}
+
+	private static class Initializer {
+		public Initializer() {}
+		Connection conn = null;
+		public void run() throws SiriusException {
+			try {
+				conn = Transaction.begin();
+
+				createDefaultProject();
+				addNodeTypes();
+				addLinkTypes();
+				addFDTypes();
+				addSensorTypes();
+				addControllerTypes();
+				addQueueControllerTypes();
+				addEventTypes();
+				addScenarioElementTypes();
+				addApplicationTypes();
+				addQuantityTypes();
+				addAggregationTypes();
+				addVehicleTypes();
+
+				Transaction.commit(conn);
+				conn = null;
+			} catch (TorqueException exc) {
+				throw new SiriusException(exc);
+			} finally {
+				if (null != conn) {
+					Transaction.safeRollback(conn);
+					conn = null;
+				}
+			}
+		}
+		private void createDefaultProject() throws TorqueException {
+			Projects db_project = new Projects();
+			db_project.setId(Long.valueOf(0));
+			db_project.setName("default");
+			db_project.save(conn);
+			logger.info("Project 'default' [id=" + db_project.getId() + "] created");
+		}
+		private void addType(BaseTypes db_type, String name) throws TorqueException {
+			db_type.setDescription(name);
+			db_type.setInUse(Boolean.TRUE);
+			db_type.save(conn);
+		}
+		private void addNodeTypes() throws TorqueException {
+			for (String name : new String[] {"freeway", "highway", "signalized_intersection", "stop_intersection", "simple", "terminal", "other"})
+				addType(new NodeTypes(), name);
+			logger.info("Added node types");
+		}
+		private void addLinkTypes() throws TorqueException {
+			for (String name : new String[] {"freeway", "highway", "onramp", "offramp", "interconnect", "HOV", "HOT", "toll", "heavy_vehicle", "bus", "street", "intersection_approach", "left_turn_pocket", "right_turn_pocket"})
+				addType(new LinkTypes(), name);
+			logger.info("Added link types");
+		}
+		private void addFDTypes() throws TorqueException {
+			for (String name : new String[] {"triangular", "trapezoidal", "linear-hyperbolic", "greenshields"})
+				addType(new FundamentalDiagramTypes(), name);
+			logger.info("Added fundamental diagram types");
+		}
+		private void addSensorTypes() throws TorqueException {
+			for (String name : new String[] {"static_point", "static_area", "moving_point"})
+				addType(new SensorTypes(), name);
+			logger.info("Added sensor types");
+		}
+		private void addControllerTypes() throws TorqueException {
+			for (String name : new String[] {"IRM_alinea", "IRM_time_of_day", "IRM_traffic_responsive", "CRM_swarm", "CRM_hero", "VSL_time_of_day", "SIG_pretimed", "SIG_actuated"})
+				addType(new ControllerTypes(), name);
+			logger.info("Added controller types");
+		}
+		private void addQueueControllerTypes() throws TorqueException {
+			for (String name : new String[] {"none", "queue_override", "proportional", "proportional_integral"})
+				addType(new QueueControllerTypes(), name);
+			logger.info("Added queue controller types");
+		}
+		private void addEventTypes() throws TorqueException {
+			for (String name : new String[] {"fundamental_diagram", "link_demand_knob", "link_lanes", "node_split_ratio", "control_toggle", "global_control_toggle", "global_demand_knob"})
+				addType(new EventTypes(), name);
+			logger.info("Added event types");
+		}
+		private void addScenarioElementTypes() throws TorqueException {
+			for (String name : new String[] {"link", "node", "network", "signal", "sensor", "controller", "event"})
+				addType(new ScenarioElementTypes(), name);
+			logger.info("Added scenario element types");
+		}
+		private void addApplicationTypes() throws TorqueException {
+			for (String name : new String[] {"estimator", "simulator", "basic_calibrator", "calibrator"})
+				addType(new ApplicationTypes(), name);
+			logger.info("Added application types");
+		}
+		private void addQuantityTypes() throws TorqueException {
+			for (String name : new String[] {"standard", "mean", "median", "STD", "1Q", "3Q", "95percentile", "min", "max", "best_case", "worst_case"})
+				addType(new QuantityTypes(), name);
+			logger.info("Added quantity types");
+		}
+		private void addAggregationTypes() throws TorqueException {
+			for (String name : new String[] {"raw", "total", "1min", "5min", "15min", "1hour", "1day"})
+				addType(new AggregationTypes(), name);
+			logger.info("Added aggregation types");
+		}
+		private void addVehicleTypes() throws TorqueException {
+			addVehicleType("general", 1.0);
+			addVehicleType("SOV", 1.0);
+			addVehicleType("HOV", 1.0);
+			addVehicleType("hybrid", 1.0);
+			addVehicleType("electric", 1.0);
+			addVehicleType("truck2", 1.5);
+			addVehicleType("truck3", 2.0);
+			addVehicleType("truck4", 3.0);
+			addVehicleType("truck5", 4.0);
+			addVehicleType("truck6", 5.0);
+			addVehicleType("bus", 1.5);
+			addVehicleType("bus2", 2.5);
+			addVehicleType("motorcycle", 0.8);
+		}
+		private void addVehicleType(String name, double weight) throws TorqueException {
+			VehicleTypes db_vt = new VehicleTypes();
+			db_vt.setDescription(name);
+			db_vt.setWeight(BigDecimal.valueOf(weight));
+			db_vt.setIsStandard(Boolean.TRUE);
+			db_vt.save(conn);
+		}
+	}
+
 }
---
+++ src/main/java/edu/berkeley/path/beats/db/BaseTypes.java
@@ -0,0 +1,13 @@
+package edu.berkeley.path.beats.db;
+
+import java.sql.Connection;
+
+import org.apache.torque.TorqueException;
+
+@SuppressWarnings("serial")
+public abstract class BaseTypes extends BaseObject {
+	public abstract void setDescription(String value);
+	public void setInUse(Boolean value) {}
+
+	public abstract void save(Connection conn) throws TorqueException;
+}
