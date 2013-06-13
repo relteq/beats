01edf26820f6969c1e44bc359881837eb08e9857
Gabriel Gomes <gomes@path.berkeley.edu>
2013-06-13 14:35:34 -0700

    cleanup after simulator test

--- src/test/java/edu/berkeley/path/beats/test/SimulatorTest.java
+++ src/test/java/edu/berkeley/path/beats/test/SimulatorTest.java
@@ -74,15 +74,15 @@ public class SimulatorTest {
 	
 	@After
 	public void clearOutput(){
-//		File file = new File(output_folder);        
-//        String[] myFiles;      
-//        if(file.isDirectory()){  
-//            myFiles = file.list();  
-//            for (int i=0; i<myFiles.length; i++) {  
-//                File myFile = new File(file, myFiles[i]);   
-//                myFile.delete();  
-//            }  
-//         }  
+		File file = new File(output_folder);        
+        String[] myFiles;      
+        if(file.isDirectory()){  
+            myFiles = file.list();  
+            for (int i=0; i<myFiles.length; i++) {  
+                File myFile = new File(file, myFiles[i]);   
+                myFile.delete();  
+            }  
+         }  
 	} 
 	
 	@Test
