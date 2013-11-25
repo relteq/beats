ec1b6376b4716b4ccd116d5a291e13bc24d36cd4
Gabriel Gomes <gomes@path.berkeley.edu>
2013-11-25 13:17:52 -0800

    Merge branch 'signals' into maxp
    Conflicts:
    	src/main/java/edu/berkeley/path/beats/control/Controller_SIG_CycleMP.java
    	src/main/resources/beats.xsd

---
+++ src/main/java/edu/berkeley/path/beats/simulator/Stage.java
@@ -0,0 +1,15 @@
+package edu.berkeley.path.beats.simulator;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: gomes
+ * Date: 11/25/13
+ * Time: 1:14 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Stage {
+
+    public Signal.NEMA movA;
+    public Signal.NEMA movB;
+
+}
